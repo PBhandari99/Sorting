a1c7edb0a43c8e5c9c5808a340a3fddeb3822156
PBhandari99 <prashantb1097@gmail.com>
2017-09-27 12:03:06 -0400

    added todo to make changes to the looping structure

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,6 +1,9 @@
 def bubble_sort(unsorted_array):
     cached_element = 0
     swapped = False 
+    # TODO: Get rid of this loop and make sure that you are not looping through the whole array
+    # every time you try to sort element as the elements towards the end are already greatests 
+    # and sorted.
     while True:
         for i in range(len(unsorted_array)-1):
             if unsorted_array[i] > unsorted_array[i+1]:
